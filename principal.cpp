e9427251ad2b246aafb95eb9da2f39908738841b
francod7777 <33075777+francod7777@users.noreply.github.com>
2018-03-10 13:14:30 -0300

    Create principal.cpp

---
+++ principal.cpp
@@ -0,0 +1,14 @@
+int main(){
+
+/* Registra un socio con su mascota. El valor el atributo racionDiaria se debe setear en 0.*/
+void registrarSocio(string ci, string nombre, const DtMascota& dtMascota){}
+
+void agregarMascota(string ci, const DtMascota& dtMascota){}
+
+void ingresarConsulta(string motivo, string ci){}
+
+DtConsulta** verConsultasAntesDeFecha(const DtFecha& Fecha, string ciSocio, int& cantConsultas){}
+
+void eliminarSocio(string ci) {}
+
+DtMascota** obtenerMascotas(string ci, int& cantMascotas){}
