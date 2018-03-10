b4d4ea3e72d7562d0a382405187669f1cb2acc8a
francod7777 <33075777+francod7777@users.noreply.github.com>
2018-03-10 13:08:00 -0300

    Update clase_socio.cpp

--- clase_socio.cpp
+++ clase_socio.cpp
@@ -1,2 +1,23 @@
-class socio
-private 
+class socio {
+private  {
+  string ci;
+  string nombre;
+  fecha fechaingreso;
+}
+  public{
+    string get_ci();
+    string get_nombre();
+    fecha get_fechaingreso();
+  }
+}
+class consulta {
+  private{
+    fecha fechaconsulta;
+    string motivo;
+  }
+  public {
+    fecha get_fechaconsulta();
+    string get_motivocons();
+  }
+}
+  
