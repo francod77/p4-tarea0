6e7d9ba02d21b87f2deb1b3375121e2d1c8f5397
francod7777 <33075777+francod7777@users.noreply.github.com>
2018-03-10 19:55:41 -0300

    Add files via upload

---
+++ enums.hpp
@@ -0,0 +1,25 @@
+
+
+enum RazaPerro
+{
+	labrador;
+	ovejero;
+	bulldog;
+	pitbull;
+	collie;
+	pekines;
+	otro;
+};
+
+enum Genero
+{
+	Macho;
+	Hembra;
+};
+
+enum TipoPelo
+{
+	Corto;
+	Mediano;
+	Largo;
+};
