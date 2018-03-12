58fe80f09cc9432c598287ccc0b4fe85e7781e08
francod7777 <33075777+francod7777@users.noreply.github.com>
2018-03-12 16:44:03 -0300

    Update VeterinariaAdministration.h

--- Veterinaria/2_BusinessLayer/BusinessComponents/VeterinariaAdministration.h
+++ Veterinaria/2_BusinessLayer/BusinessComponents/VeterinariaAdministration.h
@@ -1,9 +1,27 @@
 #ifndef VETERINARIA_VETERINARIAADMINISTRATION_H
 #define VETERINARIA_VETERINARIAADMINISTRATION_H
 
+#include "../BusinessEntities/classes/Socio.h"
+#include <string>
+#include "../../Cross-Cutting/DataTypes/DataConsulta.h"
+#include "../../Cross-Cutting/DataTypes/DataMascota.h"
+#include "../../Cross-Cutting/Const.h"
+#include "VeterinariaAdministration.cpp"
 
 class VeterinariaAdministration {
-
+private:
+    Socio* socios[MAX_SOCIOS];
+    int cantsocios=0;//cant de socios actual
+public:
+    Socio busqueda(Socio* socios,std::string ci,int cant_socios);
+    void registrarSocio(std::string ci, std::string nombre, const DataMascota& dtMascota);
+    void eliminar_socio(std::string ci);
+    void agregarMascota(std::string ci, DataMascota dtmascota);
+    void ingresarConsulta(std::string motivo,std::string ci);
+    void setcantsocios(bool agrega);//agrega uno o saca uno a cantsocios
+    int getcantsocios();
+    DataConsulta** verConsultasAntesDeFecha(const Fecha& Fecha, std::string ci, int& cantConsultas);
+    DataMascota** obtenerMascotas(std::string ci, int& cantMascotas);
 };
 
 
