ede9c5f729b9b2cf3356592a61e673d59824ca38
francod7777 <33075777+francod7777@users.noreply.github.com>
2018-03-13 10:26:14 -0300

    Update VeterinariaAdministration.cpp

--- Veterinaria/2_BusinessLayer/BusinessComponents/VeterinariaAdministration.cpp
+++ Veterinaria/2_BusinessLayer/BusinessComponents/VeterinariaAdministration.cpp
@@ -1,26 +1,57 @@
 #include "VeterinariaAdministration.h"
 
-Socio busqueda(Socio *arreglo,std::string ci,int cant){
+VeterinariaAdministration::VeterinariaAdministration() {
+    //this->socios= new Socio[MAX_SOCIOS]; ya se declara en .h
+    this->cantsocios=0;
+};
+
+VeterinariaAdministration::~VeterinariaAdministration(){};
+
+Socio VeterinariaAdministration::busqueda(std::string ci,int cant){
     int i=0;
     bool encontre=false;
     while ((!encontre && i<=Constantes.MAX_SOCIO) && i<=cant) {
-        encontre=(ci==arreglo[i].getCi());
+        encontre=(ci==this->socios[i].getCi());
         i++;
     }
     if(i<cant)
-        return arreglo[i-1];
+        return this->socios[i-1];
     else
-        return NULL;
+        return nullptr;
 };
 
 void VeterinariaAdministration::registrarSocio(std::string ci, std::string nombre, const DataMascota& dtMascota){
     Socio nuevo=new Socio(ci,nombre,fecha);
-    agregar_mascota(ci,dtMascota);//agregamos la primer mascota
-    VeterinariaAdministration.socios[VeterinariaAdministration.cantsocios]=nuevo;
-    VeterinariaAdministration.cantsocios++;
+    nuevo.agregar_Mascota(dtMascota);//agregamos la primer mascota
+    this->socios[this->cantsocios]=nuevo;
+    this->cantsocios++;
+}
+void VeterinariaAdministration::eliminar_socio(std::string ci){
+
 };
-void eliminar_socio(std::string ci);
-void agregarMascota(std::string ci, DataMascota dtmascota);
-void ingresarConsultas(std::string motivo,std::string ci);
-DataConsulta** verConsultasAntesDeFecha(const Fecha& Fecha, std::string ci, int& cantConsultas);
-DataMascota** obtenerMascotas(std::string ci, int& cantMascotas){};
+
+void VeterinariaAdministration::agregarMascota(std::string ci, DataMascota dtmascota){
+    Socio cliente = busqueda(ci,this->cantsocios);
+    if (cliente != nullptr){
+        cliente->agregar_Mascota(dtmascota);
+    }else
+        //excepcion std::invalid argument
+};
+void VeterinariaAdministration::ingresarConsulta(std::string motivo,std::string ci){
+
+};
+int VeterinariaAdministration::getcantsocios(){
+    return this->cantsocios;
+};
+
+void VeterinariaAdministration::setcantsocios(bool agrega){
+    if(agrega)  this->cantsocios++;
+    else  this->cantsocios--;
+};
+
+DataConsulta** VeterinariaAdministration::verConsultasAntesDeFecha(const Fecha& Fecha, std::string ci, int& cantConsultas){
+
+};
+DataMascota** VeterinariaAdministration::obtenerMascotas(std::string ci, int& cantMascotas){
+   
+};
