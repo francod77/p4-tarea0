6e83f18257cf23703ddf3e13b5d9c7cf3044df0d
Soyignacio <37251816+Soyignacio@users.noreply.github.com>
2018-03-16 14:08:27 -0300

    Add files via upload

--- Veterinaria/2_BusinessLayer/BusinessEntities/Classes/Socio.cpp
+++ Veterinaria/2_BusinessLayer/BusinessEntities/Classes/Socio.cpp
@@ -84,7 +84,7 @@ void Socio::agregar_Mascota(const DataMascota &mascota) {
 
 DataConsulta **Socio::getConsultasAntesDeFecha(Fecha f, int maxConsultas) {
     int cantConsultas = 0;
-    for (int i = 0; i < cantConsultas; ++i) {
+    for (int i = 0; i < this->listaConsultas->getlength(); ++i) {
         if (this->listaConsultas->get(i)->getFecha() < f) {
             cantConsultas++;
         }
@@ -93,9 +93,11 @@ DataConsulta **Socio::getConsultasAntesDeFecha(Fecha f, int maxConsultas) {
         cantConsultas = maxConsultas;
     }
     DataConsulta **res = new DataConsulta *[cantConsultas];
+    int j=0;
     for (int i = 0; i < cantConsultas; ++i) {
         if (this->listaConsultas->get(i)->getFecha() < f) {
-            res[i] = this->listaConsultas->get(i)->getDataConsulta();
+            res[j] = this->listaConsultas->get(i)->getDataConsulta();
+            j++;
         }
     };
     return res;
