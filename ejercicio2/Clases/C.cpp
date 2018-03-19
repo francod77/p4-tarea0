965f72e2576b3076d11e044b4938cd72146c3fda
felipecab <felipecabedilo@gmail.com>
2018-03-19 20:55:33 -0300

    Update C.cpp

--- ejercicio2/Clases/C.cpp
+++ ejercicio2/Clases/C.cpp
@@ -8,3 +8,18 @@ void C::print(){
     std::cout << "C: " << this->c << std::endl;
 
 }
+
+A C::getA(){
+return this->unA
+}
+
+B C::getB(){
+return this->unB
+}
+
+int C::get(){
+return this->c;
+}
+void C::set(int num){
+this->c=num;
+}
