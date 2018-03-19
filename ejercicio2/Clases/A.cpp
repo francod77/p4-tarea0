8a4e118a932d5baf6512379b4a5a79a2f3cb5fac
felipecab <felipecabedilo@gmail.com>
2018-03-19 20:45:00 -0300

    Update A.cpp

--- ejercicio2/Clases/A.cpp
+++ ejercicio2/Clases/A.cpp
@@ -2,9 +2,34 @@
 #include <iostream>
 #include "A.h"
 
-A::A(int a) : a(a){}
+void A::set(int num) {
+a=num;
+}
 
-void A::print(){
-    std::cout << "A: " << this->a << std::endl;
+int A::get() {
+    return a;
+}
+
+void A::print() {
+ std::cout<< "A" << a<< std::endl;
 
 }
+
+C A::getC() {
+    return claseC;
+}
+
+B A::getB() {
+    return claseB;
+}
+
+int A::geta() {
+    return 0;
+}
+
+A::A() : claseB(A()) {
+
+}
+
+A::A(int num, B assB, C assC) : claseB(A()) {
+a=num; claseB=assB; claseC=assC;
